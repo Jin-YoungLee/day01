35d1fd0fff5a47227c68241fdb44d3440d2b1225
Jin-YoungLee <jinyoung1734@gmail.com>
2020-11-04 16:53:32 +0900

    Day1Application 수정!

--- src/main/java/com/koreait/day01/Day01Application.java
+++ src/main/java/com/koreait/day01/Day01Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day01Application {
 
     public static void main(String[] args) {
+        System.out.println("이것이 시작점!");
+
         SpringApplication.run(Day01Application.class, args);
     }
 
